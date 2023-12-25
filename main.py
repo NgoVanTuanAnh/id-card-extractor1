cb0de92dff6399365af82c203b5d8d7ee096daef
HoVDuc <hoduccity@gmail.com>
2023-12-25 18:51:44 +0700

    update

--- main.py
+++ main.py
@@ -44,6 +44,30 @@ def getTransform(image, model):
     crop = perspective_transform(image, source_points)
     return crop
 
+# def get_info(image, image_, model, save=False):
+#     boxes, labels = get_point(image, model)
+#     info = {
+#         'id': '',
+#         'name': '',
+#         'date': ''
+#     }
+
+#     now = "{:%m-%d-%Y-%H-%M-%S}".format(datetime.datetime.now())
+#     dirname = 'id-card_' + now
+#     path = os.path.join('./testset', dirname)
+#     if not os.path.isdir(path):
+#         os.mkdir(path)
+#     Image.fromarray(image_).save(os.path.join(path, '0.jpg'))
+#     for idx, batch in enumerate(zip(boxes, labels)):
+#         bbox, label = batch
+#         x, y, w, h = bbox
+#         img = Image.fromarray(image[y:h, x-5:w+5])
+#         if save:
+#             img.save(os.path.join(path, f'{idx+1}.jpg'))
+#         # cv2.rectangle(image,(x, y),(w,h),(0,255,0),2)
+#         # cv2.putText(image, label, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+#     return image, info
+
 def get_info(image, image_, model, save=False):
     boxes, labels = get_point(image, model)
     info = {
@@ -51,23 +75,17 @@ def get_info(image, image_, model, save=False):
         'name': '',
         'date': ''
     }
-
-    now = "{:%m-%d-%Y-%H-%M-%S}".format(datetime.datetime.now())
-    dirname = 'id-card_' + now
-    path = os.path.join('./testset', dirname)
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    Image.fromarray(image_).save(os.path.join(path, '0.jpg'))
     for idx, batch in enumerate(zip(boxes, labels)):
         bbox, label = batch
         x, y, w, h = bbox
         img = Image.fromarray(image[y:h, x-5:w+5])
-        if save:
-            img.save(os.path.join(path, f'{idx+1}.jpg'))
-        # cv2.rectangle(image,(x, y),(w,h),(0,255,0),2)
-        # cv2.putText(image, label, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-    return image, info
+        pred = detector.predict(img)
+        info[label] = pred
+        cv2.rectangle(image, (x-5, y),(w+5,h),(0,255,0),2)
+        cv2.putText(image, label, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
     
+    return image, info
+
 def main(image, save_rec=True):
     image_crop = getTransform(image, model_corner)
     image_crop1 = image_crop.copy()
@@ -85,6 +103,13 @@ def GUI():
     demo.launch(share=False)
 
 if __name__ == "__main__":
+    from vietocr.tool.predictor import Predictor
+    from vietocr.tool.config import Cfg
+    config = Cfg.load_config_from_name('vgg_transformer')
+    config['cnn']['pretrained'] = False
+    config['device'] = 'cuda:0'
+    detector = Predictor(config)
+    
     model_corner = load_model_id_card()
     model_info = load_model_detect_info()
     GUI()
